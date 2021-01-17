9d535ab902b46fd2206d37c22f8ea20e71d178c7
Michael Ho <michael.ho@ieee.org>
2021-01-17 21:24:13 +0100

    Initial commit

---
+++ bandicoot.cpp
@@ -0,0 +1,5 @@
+#include "bandicoot.h"
+
+int count_bandicoots() {
+    return 0;
+}
---
+++ bandicoot.h
@@ -0,0 +1,4 @@
+#ifndef BANDICOOT_H
+#define BANDICOOT_H
+__attribute__((__visibility__("default"))) int count_bandicoots();
+#endif
